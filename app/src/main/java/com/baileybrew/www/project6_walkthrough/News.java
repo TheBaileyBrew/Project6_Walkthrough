92806a3903fc084a4ee3580ccd9641aec3467ddf
matthew <matthew.bailey@hthackney.com>
2018-07-27 16:40:51 -0400

    Initial commit

---
+++ app/src/main/java/com/baileybrew/www/project6_walkthrough/News.java
@@ -0,0 +1,28 @@
+package com.baileybrew.www.project6_walkthrough;
+
+public class News {
+    private String articleTitle, articleAuthor, articleDate, articleUrl;
+
+    public News (String articleTitle, String articleAuthor, String articleDate, String articleUrl) {
+        this.articleTitle = articleTitle;
+        this.articleAuthor = articleAuthor;
+        this.articleDate = articleDate;
+        this.articleUrl = articleUrl;
+    }
+
+    public String getArticleTitle() {
+        return articleTitle;
+    }
+
+    public String getArticleAuthor() {
+        return articleAuthor;
+    }
+
+    public String getArticleDate() {
+        return articleDate;
+    }
+
+    public String getArticleUrl() {
+        return articleUrl;
+    }
+}
